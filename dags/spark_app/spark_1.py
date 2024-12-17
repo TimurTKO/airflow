67c7d9938039f95be0f32bad6102a43b448f2974
TimurTKO <timurmadi@gmail.com>
2024-12-17 22:17:55 +0000

    Initial commit

---
+++ dags/spark_app/spark_1.py
@@ -0,0 +1,23 @@
+from pyspark.sql import SparkSession
+import os
+def run():
+    spark = SparkSession.builder.appName("ExampleJob111").getOrCreate()
+    data = [("Alice", 1), ("Bob", 2), ("Charlie", 3)]
+    df = spark.createDataFrame(data, ["Name", "Value"])
+    df.toPandas().to_csv("/opt/airflow/test/df_test.csv", index=False)
+    df = spark.read.csv("/opt/airflow/test/df_test.csv", header=True, inferSchema=True)
+    df.show()
+
+
+    #show all folders in the current directory
+    #print(os.listdir('/'))
+
+    df.show()
+    df.toPandas().to_csv("/opt/airflow/test/df_test1.csv", index=False)
+    spark.stop()
+    return
+
+
+
+if __name__ == "__main__":
+    run()
